c69ffe5eea3c8a9a4361e50dd02aec35dcc4c0be
ngoguened <tillouturnip@gmail.com>
2024-08-28 00:44:35 +0100

    migrated tests to a new file. Updated Controller class and method docstrings

---
+++ tests.py
@@ -0,0 +1,104 @@
+"""Testing"""
+import text_editor
+
+model = text_editor.Model()
+
+#check insert works correctly
+model.insert('a')
+assert model.curr_line == ['a']
+model.insert('b')
+assert model.curr_line == ['a','b']
+model.insert('c')
+assert model.curr_line == ['a','b','c']
+
+#check delete works correctly
+model.delete()
+assert model.curr_line == ['a','b']
+model.delete()
+assert model.curr_line == ['a']
+model.delete()
+assert model.curr_line == []
+model.delete()
+assert model.curr_line == []
+
+#check line caching works correctly
+model.insert('a')
+model.insert('b')
+model.insert('\n')
+assert model.curr_line == []
+assert model.lines.dict[0][0] == ['a','b']
+model.insert('c')
+model.insert('\n')
+model.insert('d')
+assert model.curr_line == ['d']
+assert model.lines.dict[0][0] == ['a','b']
+assert model.lines.dict[1][0] == ['c']
+model.left()
+model.delete()
+assert model.curr_line == ['c','d']
+assert 1 not in model.lines.dict
+model.left()
+model.delete()
+assert model.curr_line == ['a','b','c','d']
+
+model.down()
+model.insert('\n')
+model.insert('f')
+model.up()
+assert model.curr_line == ['a','b','c','d']
+while model.cursor_position != 4:
+    model.right()
+model.insert('\n')
+model.insert('e')
+assert model.curr_line == ['e']
+model.down()
+assert model.curr_line == ['f']
+
+#Check the window
+model=text_editor.Model(window_size=(1,5))
+assert model.top_window_row == 0 and model.top_window_col == 0
+for char in "Hello\nWorld!":
+    model.insert(char)
+
+assert model.top_window_col == 1
+for _ in range(6):
+    model.left()
+assert model.top_window_col == 0
+
+model.up()
+assert model.top_window_row == 0
+model.up()
+assert model.top_window_row == 0
+model.down()
+assert model.top_window_row == 1
+model.delete()
+assert model.top_window_row == 1
+model.insert('\n')
+assert model.top_window_row == 2
+model.up()
+assert model.top_window_row == 1
+
+model=text_editor.Model(window_size=(2,5))
+for char in "Hello\nthere,\nWorld!":
+    model.insert(char)
+assert model.top_window_row == 1
+model.up()
+model.up()
+assert model.top_window_row == 0
+model.down()
+for _ in range(5):
+    model.left()
+model.delete()
+assert model.top_window_row == 1
+model.insert('\n')
+assert model.top_window_row == 1
+
+
+model=text_editor.Model(window_size=(5,5))
+for char in "f\n\nf":
+    model.insert(char)
+model.left()
+model.delete()
+assert model.lines.present(model.prev_id) and model.lines.present(model.lines.fetch_next(model.prev_id))
+
+print("All tests pass\n")
--- text_editor.py
+++ text_editor.py
@@ -32,14 +32,13 @@ class Lines:
         return self.dict[key][1]
 
     def fetch_next(self, key) -> list[str]:
-            return self.dict[key][2]
+        return self.dict[key][2]
 
     def pop(self, key) -> list[list[str], int, int]:
         return self.dict.pop(key)
 
     def present(self, key) -> bool:
         return key in self.dict
-    
 
 class Model:
     """
@@ -214,12 +213,13 @@ class Model:
         self.lines.store(self.curr_id, self.curr_line, self.prev_id, self.next_id)
 
 class Controller:
-    """TODO"""
+    """The connection between the model and the view"""
     def __init__(self, model:Model=Model(),view=curses.initscr()):
         self.model = model
         self.view = view
-    
+
     def run(self):
+        "the loop connecting the model to user input, displayed using a curses view."
         self.view.keypad(True)
         curses.noecho()
         curses.cbreak()
@@ -255,113 +255,3 @@ class Controller:
         self.view.keypad(False)
         curses.echo()
         curses.endwin()
-
-def main():
-    """Testing"""
-    model = Model()
-
-    #check insert works correctly
-    model.insert('a')
-    assert model.curr_line == ['a']
-    model.insert('b')
-    assert model.curr_line == ['a','b']
-    model.insert('c')
-    assert model.curr_line == ['a','b','c']
-
-    #check delete works correctly
-    model.delete()
-    assert model.curr_line == ['a','b']
-    model.delete()
-    assert model.curr_line == ['a']
-    model.delete()
-    assert model.curr_line == []
-    model.delete()
-    assert model.curr_line == []
-
-    #check line caching works correctly
-    model.insert('a')
-    model.insert('b')
-    model.insert('\n')
-    assert model.curr_line == []
-    assert model.lines.dict[0][0] == ['a','b']
-    model.insert('c')
-    model.insert('\n')
-    model.insert('d')
-    assert model.curr_line == ['d']
-    assert model.lines.dict[0][0] == ['a','b']
-    assert model.lines.dict[1][0] == ['c']
-    model.left()
-    model.delete()
-    assert model.curr_line == ['c','d']
-    assert 1 not in model.lines.dict
-    model.left()
-    model.delete()
-    assert model.curr_line == ['a','b','c','d']
-
-    model.down()
-    model.insert('\n')
-    model.insert('f')
-    model.up()
-    assert model.curr_line == ['a','b','c','d']
-    while model.cursor_position != 4:
-        model.right()
-    model.insert('\n')
-    model.insert('e')
-    assert model.curr_line == ['e']
-    model.down()
-    assert model.curr_line == ['f']
-
-    #Check the window
-    model=Model(window_size=(1,5))
-    assert model.top_window_row == 0 and model.top_window_col == 0
-    for char in "Hello\nWorld!":
-        model.insert(char)
-    
-    assert model.top_window_col == 1
-    for _ in range(6):
-        model.left()
-    assert model.top_window_col == 0
-
-    model.up()
-    assert model.top_window_row == 0
-    model.up()
-    assert model.top_window_row == 0
-    model.down()
-    assert model.top_window_row == 1
-    model.delete()
-    assert model.top_window_row == 1
-    model.insert('\n')
-    assert model.top_window_row == 2
-    model.up()
-    assert model.top_window_row == 1
-
-    model=Model(window_size=(2,5))
-    for char in "Hello\nthere,\nWorld!":
-        model.insert(char)
-    assert model.top_window_row == 1
-    model.up()
-    model.up()
-    assert model.top_window_row == 0
-    model.down()
-    for _ in range(5):
-        model.left()
-    model.delete()
-    assert model.top_window_row == 1
-    model.insert('\n')
-    assert model.top_window_row == 1
-
-
-    model=Model(window_size=(5,5))
-    for char in "f\n\nf":
-        model.insert(char)
-    model.left()
-    model.delete()
-    assert model.lines.present(model.prev_id) and model.lines.present(model.lines.fetch_next(model.prev_id))
-
-    print("All tests pass\n")
-
-
-
-if __name__ == "__main__":
-    main()
-    
