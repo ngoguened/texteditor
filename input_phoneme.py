a85060b9aeef72773be35c6404618ce578dbdb5c
ngoguened <tillouturnip@gmail.com>
2025-03-14 11:44:21 -0400

    added current completed word to the phoneme panel.

--- input_phoneme.py
+++ input_phoneme.py
@@ -16,7 +16,7 @@ class InputPhoneme:
             if self.phonemes[0].capitalized:
                 word = word.capitalize()
             return word
-        return None
+        return ""
 
     def update_phonemes(self, char:str) -> str:
         if not char.isalpha():
@@ -30,7 +30,10 @@ class InputPhoneme:
             raise ValueError("There cannot be more than 2 values in chars.")
 
     def get_panel_text(self) -> str:
-        return ''.join([phoneme.phoneme.name for phoneme in self.phonemes]) + ''.join(self.chars)
+        curr_phonemes = ''.join([phoneme.phoneme.name for phoneme in self.phonemes])
+        curr_chars = ''.join(self.chars)
+        curr_word = self.update_word()
+        return curr_phonemes + curr_chars + "\n" + curr_word
     
     def complete(self) -> str:
         out = self.update_word()
